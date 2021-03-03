b2b153603c70d3d31fe2a2c25addab8571724867
DOMENIS\shumpei <shumpei.morimoto@ut.ee>
2021-03-03 12:38:13 +0200

    Initial commit. Created simple field, control a single sphere.

---
+++ Sticky_Ball_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+       
+        rb.AddForce(moveHorizontal * speed, 0, moveVertical * speed);
+      
+
+        
+    }
+
+    void Stop()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+}
